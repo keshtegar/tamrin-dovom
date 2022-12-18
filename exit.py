ac92068e59b1edf52463d71079054823a0f061aa
maryam keshtegar <97081304+keshtegar@users.noreply.github.com>
2022-12-18 08:48:48 +0330

    Update exit.py

--- exit.py
+++ exit.py
@@ -1,17 +1,17 @@
 #exit
 a=[]
-x=1
-while x==1:
+
+while True:
     n=int(input("please enter a number: "))
     a.append(n)
     
-    i=input(" را بزنیدy  را یزنید در غیر اینص.رت exit  اگر میخواهید خارج ش.ید ")
+    i=input(" را بزنید exit  اگر میخواهید خارج ش.ید ")
     if i=="exit":
         break
-    elif i=="y":
-        x=1
+    
+        
 sum1=sum(a)
-print("sum = ",sum1)
-
+print("sum = ",sum1)      
+    
 
     
